a5be0cc12a2b626ced62e5f776e67bf2acaacd8b
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-09 15:06:51 -0600

    added RESTFUL routes. Not saving new or create actions

--- app/controllers/client/contacts_controller.rb
+++ app/controllers/client/contacts_controller.rb
@@ -6,4 +6,60 @@ class Client::ContactsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                      first_name: params[:first_name],
+                      middle_name: params[:middle_name],
+                      last_name: params[:last_name],
+                      bio: params[:bio],
+                      email: params[:email],
+                      phone_number: params[:phone_number]
+                      }
+    response = HTTP.post(
+                          "http://localhost:3000/api/contacts",
+                          form: client_params
+                          )
+    contact = response.parse
+    redirect_to "/client/contacts/#{ contact["id"] }"
+  end
+
+  def show
+    response = HTTP.get("http://localhost:3000/api/contacts/#{params[:id]}")
+    @contact = response.parse
+    render 'show.html.erb'
+  end
+
+  def edit
+    response = HTTP.get("http://localhost:3000/api/contacts/#{params[:id]}")
+    @contact = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    client_params = {
+                  first_name: params[:first_name],
+                  middle_name: params[:middle_name],
+                  last_name: params[:last_name],
+                  group: params[:group],
+                  bio: params[:bio],
+                  email: params[:email],
+                  phone_number: params[:phone_number]
+                  }
+    response = HTTP.patch(
+                          "http://localhost:3000/api/contacts/#{params[:id]}",
+                          form: client_params
+                          )
+    contact = response.parse
+    redirect_to "/client/contacts/#{ contact["id"] }"
+  end
+
+  def destroy
+    response = HTTP.delete("http://localhost:3000/api/contacts/#{params[:id]}")
+    redirect_to '/client/contacts'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :client do
    get '/contacts' => 'contacts#index' 
+   get '/contacts/new' => 'contacts#new'
+   post '/contact' => 'contacts#create'
+   get '/contacts/:id' => 'contacts#show'
+   get '/contacts/:id/edit' => 'contacts#edit'
+   patch '/contacts/:id' => 'contacts#update'
+   delete '/contacts/:id' => 'contacts#destroy'
   end
 end
